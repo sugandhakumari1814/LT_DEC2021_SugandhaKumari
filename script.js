dde41c3bd562fc022e2da88c66f62e63c7106e4a
Sugu <72254031+sugandhakumari1814@users.noreply.github.com>
2021-12-25 12:36:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Owl carousel script
+$(document).ready(function () {
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            300: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
